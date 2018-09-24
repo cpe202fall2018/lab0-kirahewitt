c6a94b442c996160e5abbe46a063c074266e8a23
Kira Hewitt <kira@Kiras-Macbook-Air.local>
2018-09-24 13:46:48 -0700

    update

--- planets.py
+++ planets.py
@@ -8,5 +8,5 @@ def weight_on_planets():
     #Prints out results
     print("\nOn Mars you would weigh", mars, "pounds.\nOn Jupiter you would weigh", jupiter, "pounds.")
 
-    if __name__ == '__main__':
-        weight_on_planets()
+if __name__ == '__main__':
+    weight_on_planets()
